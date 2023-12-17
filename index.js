efbf3204100c3933b002788e36051332b1b7ecba
HIL PATEL <145318878+Hil-Patel@users.noreply.github.com>
2023-12-17 17:27:24 +0530

    Update index.js
    updating javascript and using concatenation

--- index.js
+++ index.js
@@ -1,41 +1,6 @@
 function assign(n,op){
-    if(n==1){
-        document.querySelector(".img1").setAttribute("src","./images/dice1.png");
-    }
-    else if(n==2){
-        document.querySelector(".img1").setAttribute("src","./images/dice2.png");
-    }
-    else if(n==3){
-        document.querySelector(".img1").setAttribute("src","./images/dice3.png");
-    }
-    else if(n==4){
-        document.querySelector(".img1").setAttribute("src","./images/dice4.png");
-    }
-    else if(n==5){
-        document.querySelector(".img1").setAttribute("src","./images/dice5.png");
-    }
-    else if(n==6){
-        document.querySelector(".img1").setAttribute("src","./images/dice6.png");
-    }
-
-    if(op==1){
-        document.querySelector(".img2").setAttribute("src","./images/dice1.png");
-    }
-    else if(op==2){
-        document.querySelector(".img2").setAttribute("src","./images/dice2.png");
-    }
-    else if(op==3){
-        document.querySelector(".img2").setAttribute("src","./images/dice3.png");
-    }
-    else if(op==4){
-        document.querySelector(".img2").setAttribute("src","./images/dice4.png");
-    }
-    else if(op==5){
-        document.querySelector(".img2").setAttribute("src","./images/dice5.png");
-    }
-    else if(op==6){
-        document.querySelector(".img2").setAttribute("src","./images/dice6.png");
-    }
+    document.querySelector(".img1").setAttribute("src","./images/dice"+n+".png");
+    document.querySelector(".img2").setAttribute("src","./images/dice"+op+".png");
 }
 var num1=Math.floor(Math.random()*6)+1;
 var num2=Math.floor(Math.random()*6)+1;
